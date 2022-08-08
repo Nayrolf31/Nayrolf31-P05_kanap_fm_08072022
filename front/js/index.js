f038d2b7b0d6dc2f038979333bea15f9088bbd43
florian maugan <florian.maugan@gmail.com>
2022-08-08 14:35:46 +0200

    rework

--- front/js/index.js
+++ front/js/index.js
@@ -1,51 +1,92 @@
-/* création fetch */
+//création d'un fetch pour récupérer les données API
 
-let allProduct = [];
-
-const fetchProduct = async () => {
-  await fetch("http://localhost:3000/api/products")
-    .then((res) => res.json())
-    .then((promise) => {
-      allProduct = promise;
-      console.log(allProduct);
-    });
-};
+const url = "http://localhost:3000/api/products"
+fetch(url)
+  .then(res => res.json())
+  .then(data => addProducts(data))
 
 
-//définition d'une const pour récupérer les données de l'API
+//création d'une fonction pour récupérer les produits
 
-const productDisplay = async () => {
-  await fetchProduct();
+function addProducts(produits) {
 
-  try {
-    document.getElementById("items",).innerHTML = allProduct.map((product) => `
-    <a class="button" id="${product._id}" href="./product.html?id=${product._id}">
-      <article >
-        <img class="productImg" src="${product.imageUrl}" alt="${product.altTxt}">
-        <h3 class="productTitle">${product.name}</h3>
-        <p class="productDescription">${product.description}</p>
-       <!--  <div class="productColor">${product.colors}</div>
-        <span class="productPrice">${product.price} €</span> -->
-      </article>
-    </a>
-  `).join("");
-  } catch (e) {
-    console.log("il y à une erreur" + e)
+  //const _id = data[0]._id
+  //const imageUrl = data[0].imageUrl
+  //const altTxt = data[0].altTxt
+  //const name = data[0].name
+  //const description = data[0].description
+
+  //mise en place Loop pour récupérer tout les produits
+
+  produits.forEach((produit) => {
+    console.log("produit", produit)
+
+//appel de la fonction makeanchor
+
+  const { _id, imageUrl, altTxt, name, description } = produit
+  const anchor = makeAnchor(_id);
+  const article = document.createElement("article")
+  const image = makeImage(imageUrl, altTxt)
+  const h3 = makeH3(name)
+  const p = makeParagraphe(description)
+
+  appendElementsToArticle(article, image, h3, p)
+  appendArticleToAnchor(anchor, article)
+});
+}
+
+function appendElementsToArticle(article, image, h3, p) {
+  article.appendChild(image)
+  article.appendChild(h3)
+  article.appendChild(p)
+
+}
+
+function makeAnchor(id) {
+  const anchor = document.createElement("a");
+  anchor.href = "./product.html?id=" + id
+  return anchor
+}
+
+function appendArticleToAnchor(anchor, article) {
+  const items = document.querySelector("#items")
+  if (items != null) {
+    items.appendChild(anchor)
+    anchor.appendChild(article)
+    console.log("élément ajouté à items", items)
   }
+}
 
-  //mise en place d'une variable pour accéder à la page produit
+//fonction pour récup img
 
-  let buttons = document.querySelectorAll(".button");
-  console.log(buttons)
+function makeImage(imageUrl, altTxt) {
+  const image = document.createElement("img")
+  image.src = imageUrl
+  image.alt = altTxt
+  return image
+}
 
-  buttons.forEach((button) =>
-    button.addEventListener("click", () => {
-      console.log(button);
+//fonc recup name
 
-      window.location = `product.html?${button.id}`;
-    }),
-  );
+function makeH3(name) {
+  const h3 = document.createElement("h3")
+  h3.textContent = name
+  h3.classList.add("productName")
+  return h3
+}
 
-};
+//fonc recup P
 
-productDisplay();
+function makeParagraphe(description) {
+  const p = document.createElement("p")
+  p.textContent = description
+  p.classList.add("productDescription")
+  return p
+
+}
+
+
+
+
+
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,109 +1,68 @@
+//utilisation de urlsearchparams pour récupérer l'id du produit
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString)
+const id = urlParams.get("id")
+
+fetch(`http://localhost:3000/api/products/${id}`)
+    .then(response => response.json())
+    .then(res => handleData(res))
 
 
-//Utilisation de URLSearchParams pour récupérer les id produits pour la page produit.
+//fontion pour récupérer les infos produit
 
-let params = new URLSearchParams(document.location.search);
-let idParams = params.get("id");
+function handleData(produit) {
+    console.log({ produit })
 
-const product = window.location.search.split("?id=").join("");
-console.log(product);
+    const { altTxt, colors, description, imageUrl, name, price } = produit
+    makeImage(imageUrl, altTxt)
+    makeTitle(name)
+    makePrice(price)
+    makeDescription(description)
+    makeColors(colors)
 
-let allProduct = [];
-
-//Utilisation d'un fetch pour récupérer les produits
-//un par un sous forme d'objets.
-
-const fetchProduct = async () => {
-  await fetch(`http://localhost:3000/api/products/${idParams}`)
-    .then((res) => res.json())
-    .then((promise) => {
-      allProduct = promise;
-      console.log(allProduct);
-    });
-};
-
-//constante pour afficher les éléments de la page
-
-const productDisplay = async () => {
-  await fetchProduct();
-
-try{
-  document.querySelector(".item__img").innerHTML = `<img src=${allProduct.imageUrl} alt=${allProduct.altTxt}>`;
-  document.getElementById("title").textContent = `${allProduct.name}`;
-  document.getElementById("price").textContent = `${allProduct.price}`;
-  document.getElementById("description").textContent = `${allProduct.description}`;
- 
-  //affichage de la couleur dans les options
-
-  let select = document.getElementById("colors")
-  console.log(select);
-
-  console.log(allProduct.colors);
-
-  allProduct.colors.forEach((color) => {
-    console.log(color)
-    let tagOption = document.createElement("option");
-
-    tagOption.innerHTML = `${color}`;
-
-    select.appendChild(tagOption)
-    console.log(tagOption)
-  });
-  
-  addBasket(allProduct)
-  
-} catch (e) {
-  console.log("il y à une erreur" + e)
+    //const altTxt = produit.altTxt
+    //const colors = produit.colors
+    //const description = produit.description
+    //const name = produit.name
+    //const price = produit.price
+    //const _id = produit._id
 }
-};
 
-productDisplay();
+//func recup infos produit ( img, alt, title, price etc..)
 
-//constante pour activer le bouton d'ajout au panier
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement('img')
+    image.src = imageUrl
+    image.alt = altTxt
+    const parent = document.querySelector(".item__img")
+    if (parent != null) parent.appendChild(image)
+}
 
-/*document.getElementById('addToCart').id = '${idParams._id}';
+function makeTitle(name) {
+    const h1 = document.querySelector("#title")
+    if (h1 != null) h1.textContent = name
+}
 
-const addBasket = (allProduct) => {
-  let bouton = document.getElementById(allProduct._id);
-  console.log(bouton);
-  bouton.addEventListener("click", () => {
-    let select = document.getElementById("colors")
-    console.log(select)
-  })
-}*/
+function makePrice(price) {
+    const span = document.querySelector("#price")
+    if (span != null) span.textContent = price
+}
 
-//constante qui permet d'envoyer les données dans le localstorage
+function makeDescription(description) {
+    const p = document.querySelector("#description")
+    if (p != null) p.textContent = description
+}
 
-const addBasket = (allProduct) => {
-  document.getElementById(addToCart);
-  console.log(addToCart);
-  addToCart.addEventListener("click", () => {
-    let produitTableau = JSON.parse(localStorage.getItem("product"));
-    let select = document.getElementById("colors");
-    /*let imageUrl, altTxt;
-    let price*/
-    console.log(select.value);
-    /*console.log(produitTableau);
-    console.log(imageUrl, altTxt);
-    console.log(price);*/
-
-
-    //constante qui permet de récupéré la couleur choisie
-
-    const fusionProduitTeinte = Object.assign({} , allProduct, {
-      teinte : `${select.value}`,
-      quantite: 1, 
-    });
-    console.log(fusionProduitTeinte);
-
-    if(produitTableau == null) {
-      produitTableau = []
-      produitTableau.push(fusionProduitTeinte);
-      console.log(produitTableau)
-      localStorage.setItem("product",JSON.stringify(produitTableau));
+function makeColors(colors) {
+    const select = document.querySelector("#colors")
+    if (select != null) {
+        colors.forEach((color) => {
+            const option = document.createElement("option")
+            option.value = color
+            option.textContent = color
+            select.appendChild(option)
+        });
     }
-  })
 }
-
-
 
